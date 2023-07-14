c555c139c6ddf38f8e40f502fa9c55041bd52ec8
Bakrim Aamr <aamr.siimaroc@gmail.com>
2023-07-14 17:14:56 +0000

    Update Footer.js

--- react/blogapi/src/components/Footer.js
+++ react/blogapi/src/components/Footer.js
@@ -28,7 +28,7 @@ function Copyright() {
 			</Link>{' '}
 			{new Date().getFullYear()}
 			{'.'}
-			 -Modified by <em>Haitam Elkadiri</em>
+			 -Modified by <em>SII MAROC</em>
 		</Typography>
 	);
 }
