34004b3d571bfcb1d58d277c8b49a4d16dcf3f53
Bakrim Aamr <aamr.siimaroc@gmail.com>
2023-07-14 17:08:19 +0000

    Test CI CD 14/07/2023

--- django/core/settings.py
+++ django/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+#test CI CD 14/07/2023 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
